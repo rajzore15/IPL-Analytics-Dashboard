45e1e9df0931cc28af7cf6927482edf2a6384b8a
rajzore15 <raj.zore24@pcu.edu.in>
2026-06-04 16:25:51 +0530

    Update project dependencies and fix Streamlit app imports

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 
 # ---------------- PAGE CONFIG ----------------
